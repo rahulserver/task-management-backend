324481e34cb925fee1beeaa8a57913b9444bf454
rahulserver <rahulserver@gmail.com>
2025-01-26 14:09:09 +0530

    add firebase configuration serviceAccount.json to gitignore and access it in firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,12 @@
 import * as admin from 'firebase-admin';
+import * as path from 'path';
+
+const serviceAccountPath = path.resolve(__dirname, '../../serviceAccount.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    // Add other Firebase config options as needed
+    credential: admin.credential.cert(require(serviceAccountPath)),
+    databaseURL: 'https://task-management-affworld.firebaseio.com', // Replace with your database URL
   });
 }
-
 export default admin;
